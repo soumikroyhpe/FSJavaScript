a7ae77865a0eba5a7098b297a6e1c6c00de90be9
soumikroyhpe <soumik.royhpe@gmail.com>
2025-08-16 19:32:41 +0100

    Day2 final changes

---
+++ day2.js
@@ -0,0 +1,18 @@
+firstname = "Soumik"
+
+console.log("firstname :" + firstname);
+console.log("firstname datatype: "+ typeof firstname) // string
+firstname = 78
+console.log("firstname :" + firstname);
+console.log("firstname datatype: "+ typeof firstname) //number
+
+firstname = true
+console.log("firstname :" + firstname);
+console.log("firstname datatype: "+ typeof firstname) //boolean
+
+
+secondname = undefined
+console.log("secondname datatype: "+ typeof secondname) //undefined
+
+thirdName = null
+console.log("thirdName datatype: "+ typeof thirdName) // object
